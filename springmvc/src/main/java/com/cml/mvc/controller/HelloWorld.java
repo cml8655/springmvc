a1b269e73c75823d5405d49925cb571df58e6183
cmlbeliever <851688870@qq.com>
2016-04-18 15:55:41 +0800

    update

--- springmvc/src/main/java/com/cml/mvc/controller/HelloWorld.java
+++ springmvc/src/main/java/com/cml/mvc/controller/HelloWorld.java
@@ -73,7 +73,7 @@ public class HelloWorld {
 	}
 
 	@ExceptionHandler
-	public @ResponseBody String handle(IllegalStateException e) {
+	public @ResponseBody String handle(Exception e) {
 		return "IllegalStateException handled!";
 	}
 
